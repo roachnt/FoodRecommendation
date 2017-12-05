124b25d5dae6db962270b25d738c16df0d6ceac2
Nick Roach <roachnt@miamioh.edu>
2017-12-05 13:41:42 -0500

    finish food search/fatsecret api integration

--- routes/food.js
+++ routes/food.js
@@ -3,30 +3,34 @@ var router = express.Router();
 const fatAPI = new (require('fatsecret'))('2daa4fc48c494e61b82e1d567f0df576', '1a9420e477594bff82475096785f26fa');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('food');
+router.get('/', function (req, res, next) {
+    res.render('food');
 });
 
-router.post('/search', function(req, res, next) {
-    var food = req.body.food;
+router.post('/search', function (req, res, next) {
+    var food = req.body.food.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
     fatAPI
-    .method('foods.search', {
-      search_expression: 'Burger',
-      max_results: 20
-    })
-    .then(function(results) {
-        var descriptions = [];
-        var foods = results.foods.food;
-        foods.forEach(food => {
-            var description = food.food_description.split('|');
-            description[0] = description[0].split('-')[1];
-            descriptions.push(description);
-        });
-        res.render('search', {results: results.foods.food, descriptions: descriptions});
-    })
-    .catch(err => console.error(err));
+        .method('foods.search', {
+            search_expression: food,
+            max_results: 20
+        })
+        .then(function (results) {
+            var descriptions = [];
+            var foods = results.foods.food;
+            console.log(foods);
+            foods.forEach(food => {
+                var description = food.food_description.split('|');
+                description[0] = description[0].split('-')[1];                
+                for (var i = 0; i < description.length; i++) {
+                    description[i] = parseFloat(description[i].replace(/[^\d.-]/g, ''));
+                }
+                descriptions.push(description);
+            });
+            res.render('search', { results: results.foods.food, descriptions: descriptions });
+        })
+        .catch(err => console.error(err));
 
-    
+
 });
 
 module.exports = router;
